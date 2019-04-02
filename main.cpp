04b0f34760457e9474da841eeb3bb21431f3b99c
Torgee <torge.herber@gmail.com>
2019-04-02 21:50:12 +0200

    updating c++ standard to cxx17

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 
     QQmlApplicationEngine engine;
     qmlRegisterSingletonType<FileActions>(
-                "MyStuff", 1, 0, "FileActions", [](QQmlEngine *, QJSEngine *) -> QObject* {return new FileActions{nullptr};}
+                "MyStuff", 1, 0, "FileActions", [](auto *, auto *) -> QObject* {return new FileActions{nullptr};}
     );
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     if (engine.rootObjects().isEmpty())
